1d7b8902e85a8c86489ca72c66f22510b94cc5e3
Tanki <tanki_jong@hotmail.com>
2015-12-10 23:35:57 +0800

    compile css

--- themes/learn4/js/addTargetToAnchors.js
+++ themes/learn4/js/addTargetToAnchors.js
@@ -1,8 +1,10 @@
 //这个脚本的作用是让所有的链接在新页面打开。
 //由于markdown不能给a元素加target属性，且把所有的链接用html重写太麻烦，所以就用这个办法。
-$(function(){
-	var myAnchors=$("#body a")
-	for (var i = myAnchors.length - 1; i >= 0; i--) {
-		$(myAnchors[i]).attr('target','_blank')
-	};
-})
+//
+//2015.12.10  by tanki.  交互上希望不要跳转翻页  所以注释了。
+// $(function(){
+// 	var myAnchors=$("#body a")
+// 	for (var i = myAnchors.length - 1; i >= 0; i--) {
+// 		$(myAnchors[i]).attr('target','_blank')
+// 	};
+// })
