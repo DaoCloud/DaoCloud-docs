c568b8513c1d6d2084434f0cbe5ec37b569cf0ce
Tanki <tanki_jong@hotmail.com>
2015-12-13 15:09:17 +0800

    update navigation, unflod when click

--- themes/learn4/js/addTargetToAnchors.js
+++ themes/learn4/js/addTargetToAnchors.js
@@ -1,6 +1,7 @@
 //这个脚本的作用是让所有的链接在新页面打开。
 //由于markdown不能给a元素加target属性，且把所有的链接用html重写太麻烦，所以就用这个办法。
 //
+//
 //2015.12.10  by tanki.  交互上希望不要跳转翻页  所以注释了。
 // $(function(){
 // 	var myAnchors=$("#body a")
@@ -8,3 +9,13 @@
 // 		$(myAnchors[i]).attr('target','_blank')
 // 	};
 // })
+
+//导航栏折叠功能
+$(function(){
+	var links = $('#sidebar ul.topics>li>a');
+	links.removeAttr('href');
+	links.on('click',function(e){
+		$('#sidebar ul.topics>li>ul').removeClass('open');
+		$(e.target).parents('li').find('ul').addClass('open');
+	})
+})
