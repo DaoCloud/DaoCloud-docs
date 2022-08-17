0016f066a54b809f5dfe15a4a400ba771636bec0
samzong.lu <samzong.lu@gmail.com>
2022-08-17 21:12:19 +0800

    init ucloud s3 upload script

---
+++ scripts/upload-ucloud.py
@@ -0,0 +1,6 @@
+# 密钥可在https://console.ucloud.cn/uapi/apikey中获取
+public_key = 'TOKEN_fb1355c7-6970-4c00-83a3-d58cd4d1bf07'  # 账户公钥
+private_key = '72cfac06-b7fb-4049-9123-a41219a00268'  # 账户私钥
+
+bucket = 'community-github'  # 空间名称
+upload_host = '.cn-sh2.ufileos.com'
