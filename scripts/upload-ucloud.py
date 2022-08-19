2d60487288d6254444840aeea790da6d7e66a95a
samzong.lu <samzong.lu@gmail.com>
2022-08-19 17:23:19 +0800

    add gitlab-ci

--- scripts/upload-ucloud.py
+++ scripts/upload-ucloud.py
@@ -1,6 +1,54 @@
-# 密钥可在https://console.ucloud.cn/uapi/apikey中获取
-public_key = 'TOKEN_fb1355c7-6970-4c00-83a3-d58cd4d1bf07'  # 账户公钥
-private_key = '72cfac06-b7fb-4049-9123-a41219a00268'  # 账户私钥
+# /usr/bin/env python3
+# -*- coding: UTF-8 -*-
 
-bucket = 'community-github'  # 空间名称
-upload_host = '.cn-sh2.ufileos.com'
+
+"""
+
+Author: samzong.lu
+E-mail: samzong.lu@gmail.com
+
+"""
+
+import logging
+import os
+import sys
+import subprocess
+
+
+def do_upload_cli(file, public_key, private_key, region):
+    cmd = '../scripts/tools/us3cli-linux64'
+
+    if not os.path.exists(cmd):
+        print('can not find us3cli command')
+        return 'error'
+
+    subprocess.run(['chmod', '+x', cmd])
+
+    if os.path.isdir(file):
+        subprocess.run(
+            [cmd, 'cp', '-r', file, 'us3://community-github', '--accesskey', public_key, '--secretkey',
+             private_key,
+             '--endpoint',
+             region])
+    elif os.path.isfile(file):
+        subprocess.run(
+            [cmd, 'cp', file, 'us3://community-github', '--accesskey', public_key, '--secretkey', private_key,
+             '--endpoint',
+             region])
+
+
+if __name__ == '__main__':
+    argv = sys.argv
+
+    if len(argv) != 4:
+        logging.error('args not right')
+    else:
+        public_key = argv[1].strip('public_key=')
+        private_key = argv[2].strip('private_key=')
+        region = argv[3].strip('region=')
+
+        logging.info(public_key + private_key + region)
+
+        for file in os.listdir():
+            logging.info(file)
+            print(do_upload_cli(file, public_key, private_key, region))
