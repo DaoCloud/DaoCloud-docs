d1eebb46dd2fb712095891906037f1216cc3c4ae
samzong.lu <samzong.lu@gmail.com>
2022-08-21 18:39:50 +0800

    Update gitlab ci

--- scripts/alert_webhook.py
+++ scripts/alert_webhook.py
@@ -8,22 +8,24 @@ Author: samzong.lu
 E-mail: samzong.lu@gmail.com
 
 """
+
 import json
+import logging
 
 import requests
 
 
-def alter_webhook(message: str = None):
+def alter_webhook(bucket: str = None, message: str = None):
     webhook_url = "https://api.ssp.cloud.tencent.com/engine/webhook/31/1560638495413972994"
 
     payload = json.dumps({
-        "result": message or "error"
+        "result": "{} : {}".format(bucket or "null", message or "error")
         })
 
     resp = requests.post(url=webhook_url, data=payload)
 
-    print(resp.json())
+    logging.info(resp.json())
 
 
 if __name__ == '__main__':
-    alter_webhook(message="success")
+    alter_webhook(bucket="community-offical", message="error")
--- scripts/upload-ucloud.py
+++ scripts/upload-ucloud.py
@@ -18,24 +18,26 @@ import time
 from alert_webhook import alter_webhook
 
 
-def do_upload_cli(file, public_key, private_key, region):
+def do_upload_cli(file, public_key, private_key, region, bucket):
     cmd = '../scripts/tools/us3cli-linux64'
 
+    bucket = 'us3://' + bucket
+
     if not os.path.exists(cmd):
-        print('can not find us3cli command')
+        logging.error('can not find us3cli command')
         return 'error'
 
     subprocess.run(['chmod', '+x', cmd])
 
     if os.path.isdir(file):
         out = subprocess.run(
-            [cmd, 'cp', '-r', file, 'us3://community-github', '--accesskey', public_key, '--secretkey',
+            [cmd, 'cp', '-r', file, bucket, '--accesskey', public_key, '--secretkey',
              private_key,
              '--endpoint',
              region])
     elif os.path.isfile(file):
         out = subprocess.run(
-            [cmd, 'cp', file, 'us3://community-github', '--accesskey', public_key, '--secretkey', private_key,
+            [cmd, 'cp', file, bucket, '--accesskey', public_key, '--secretkey', private_key,
              '--endpoint',
              region])
 
@@ -46,22 +48,23 @@ def do_upload_cli(file, public_key, private_key, region):
 if __name__ == '__main__':
     argv = sys.argv
 
-    if len(argv) != 4:
-        logging.error('args not right')
+    if len(argv) != 5:
+        raise Exception('args not right', argv)
     else:
-        public_key = argv[1].strip('public_key=')
-        private_key = argv[2].strip('private_key=')
-        region = argv[3].strip('region=')
+        public_key = argv[1].split('=')[1]
+        private_key = argv[2].split('=')[1]
+        region = argv[3].split('=')[1]
+        bucket = argv[4].split('=')[1]
 
-        logging.info(public_key + private_key + region)
+        logging.debug(public_key + private_key + region + bucket)
 
         try:
             for file in os.listdir():
                 logging.info(file)
-                do_upload_cli(file, public_key, private_key, region)
+                do_upload_cli(file, public_key, private_key, region, bucket)
 
-            alter_webhook(message="success")
+            alter_webhook(bucket, message="success")
 
         except Exception as e:
             logging.error(e)
-            alter_webhook(message="error")
+            alter_webhook(bucket, message="error")
