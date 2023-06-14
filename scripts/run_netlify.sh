3ffbe27834d159d34c9b4c90ca78424066cf6ccd
samzong.lu <samzong.lu@gmail.com>
2023-06-14 15:17:23 +0800

    feat: add pr preview tool netlify.app
    Signed-off-by: samzong.lu <samzong.lu@gmail.com>
    
    feat: add pr preview tool netlify.app
    
    Signed-off-by: samzong.lu <samzong.lu@gmail.com>

---
+++ scripts/run_netlify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# check os type
+
+# install python3
+
+# install pip
+
+# install required packages
+pip install mkdocs-material \
+    mkdocs-git-revision-date-plugin \
+    mkdocs-mermaid2-plugin \
+    mkdocs-rss-plugin \
+    mkdocs-minify-plugin \
+    mkdocs-macros-plugin \
+    mkdocs-git-revision-date-localized-plugin \
+    mkdocs-awesome-pages-plugin \
+    mkdocs-redirects \
+    mkdocs-print-site-plugin \
+    mkdocs-swagger-ui-tag \
+    pyyaml
+
+
+# build the site
+mkdocs build -f docs/zh/mkdocs.yml -d ../../public/
+mkdocs build -f docs/en/mkdocs.yml -d ../../public/en/
