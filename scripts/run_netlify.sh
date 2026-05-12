19fca1dfc439fb017938b8e99cdbc88c07b54629
samzong <samzong.lu@gmail.com>
2026-05-12 13:29:51 +0800

    fix(netlify): always build docs preview
    Signed-off-by: samzong <samzong.lu@gmail.com>

--- scripts/run_netlify.sh
+++ scripts/run_netlify.sh
@@ -1,48 +1,6 @@
 #!/bin/bash
 
-# run pull request preview site with netlify
+set -euo pipefail
 
 make sync
-
-# Ensure the git repository is up to date.
-git fetch origin main
-
-# Get the current build commit SHA
-CURRENT_COMMIT_SHA=$(git rev-parse HEAD)
-echo "Current commit SHA: $CURRENT_COMMIT_SHA"
-
-# Get the latest commit SHA of the main branch
-MAIN_COMMIT_SHA=$(git rev-parse origin/main)
-echo "Main commit SHA: $MAIN_COMMIT_SHA"
-
-# Get the list of changed files and convert it to a space-separated string.
-CHANGED_FILES=$(git diff --name-only $MAIN_COMMIT_SHA $CURRENT_COMMIT_SHA | tr '\n' ' ')
-
-# Add the list of modified files to the environment variable.
-export CHANGED_FILES
-
-# Print environment variables to confirm
-echo "CHANGED_FILES environment variable:"
-echo "$CHANGED_FILES"
-
-# Generate the navigation for the docs
-make generate-nav
-
-# Perform different operations based on the modified files.
-if echo "$CHANGED_FILES" | grep -q 'docs/zh/docs/'; then
-  echo "Chinese docs were modified, running make build zh..."
-  make build zh
-fi
-
-if echo "$CHANGED_FILES" | grep -q 'docs/en/docs/'; then
-  echo "English docs were modified, running make build en..."
-  make build en
-fi
-
-# Check if the public/ directory exists, and create it if it does not.
-if [ ! -d "public" ]; then
-  echo "Public directory not found. Creating public/ directory."
-  mkdir -p public/en/
-  echo "<html><body><h1>Welcome to the public directory</h1></body></html>" > public/index.html
-  echo "<html><body><h1>Welcome to the public directory</h1></body></html>" > public/en/index.html
-fi
+make build all
